9addc4b339eda7d8d1b06233bf8127019eebaeb2
Eduardo <emp9506@gmail.com>
2021-09-14 15:30:01 -0400

    Init

--- python_api/wsgi.py
+++ python_api/wsgi.py
@@ -1,4 +1,4 @@
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'superalba_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'python_api.settings')
 application = get_wsgi_application()
